6d46490eeddcdb9e22c8efc5ab6199e7cd8f82b3
Meena Uppada <meenauppada971@gmail.com>
2025-01-16 14:29:36 +0530

    changes in projects data file

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -1,13 +1,14 @@
-import simon from '../images/simon.png'
+import blog from '../images/blog.webp'
 import Todo from '../images/Todo.jpg'
 import WeatherApp from '../images/weatherApp.png'
+import simon from '../images/simon.png'
 const ProjectData = [
     {
-        src: simon,
-        title : "Simon Game",
-        text : "The Simon Game is a classic memory challenge where players replicate sequences of lights and sounds, testing memory and concentration.",
-        view: "https://meena51.github.io/simongame/",
-        source : "https://github.com/meena51/simongame"
+        src: blog,
+        title : "Blog Website",
+        text : "A modern blog website built with React and powered by Contentful CMS. It enables seamless content management, dynamic rendering, and a responsive user experience for blog enthusiasts.",
+        view: "https://github.com/meena51/blog",
+        source : "https://github.com/meena51/blog"
     },
     {
         src: Todo,
@@ -22,6 +23,13 @@ const ProjectData = [
         text : "A Weather Application provides real-time weather updates, forecasts, and alerts, helping users plan their day and stay informed about conditions.",
         view: "https://github.com/meena51/Weather-App",
         source : "https://github.com/meena51/Weather-App"
+    },
+    {
+        src: simon,
+        title : "Simon Game",
+        text : "The Simon game built with HTML, CSS, and JavaScript challenges players to repeat a growing sequence of colors. It showcases basic web development skills, including DOM manipulation, event handling, and state management.",
+        view: "https://meena51.github.io/simongame/",
+        source : "https://github.com/meena51/simongame"
     }
 ]
 export default ProjectData;
